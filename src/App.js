b188140c6052de47f0c9f95f7afe476bfa0748a1
jmetz9 <jmetz9@my.wctc.edu>
2023-02-06 00:19:03 -0600

    next commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Country from './components/Country';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    countries: [
+      { id: 1, country: 'United States', goldMedalCount: 2 },
+      { id: 2, country: 'China', goldMedalCount: 3 },
+      { id: 3, country: 'Germany', goldMedalCount: 0 },
+    ]
+  }
+  handleIncrement = (countryId) => {
+    const country = this.state.countries.find(c => c.id === countryId);
+    //this.setState({goldMedalCount: country.goldMedalCount += 1})
+    this.setState({goldMedalCount: country.goldMedalCount += 1})
+  }
+  handleReduction = (countryId) => {
+    const country = this.state.countries.find(c => c.id === countryId);
+    this.setState({goldMedalCount: country.goldMedalCount -= 1})
+    }
+  render() {
+    return (
+      <div className="App">
+        { this.state.countries.map( country =>
+          <Country
+          key = {country.id}
+          country = {country}
+          onIncrement = {this.handleIncrement}
+          onReduction = {this.handleReduction}
+        />)}
+      </div>
+    );
+  }
 }
 
 export default App;
